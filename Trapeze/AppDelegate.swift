19d02fb95949ae00e832369783fd7a6e3d6c13c9
sandpiturtle <sandpiturtle@mail.ru>
2017-05-28 23:44:57 +0300

    Initialize repository

---
+++ Trapeze/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Trapeze
+//
+//  Created by Nikita Pokidyshev on 28.05.17.
+//  Copyright © 2017 Nikita Pokidyshev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
+
---
+++ Trapeze/SceneVC.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Trapeze
+//
+//  Created by Nikita Pokidyshev on 28.05.17.
+//  Copyright © 2017 Nikita Pokidyshev. All rights reserved.
+//
+
+import UIKit
+
+class SceneVC: UIViewController {
+
+}
+
