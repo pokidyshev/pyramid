082ab48a8ef95bd7e6be224c61e30bd9c009dd45
sandpiturtle <sandpiturtle@mail.ru>
2017-05-29 00:09:48 +0300

    Add setup code

--- Trapeze/SceneVC.swift
+++ Trapeze/SceneVC.swift
@@ -7,8 +7,48 @@
 //
 
 import UIKit
+import Metal
 
 class SceneVC: UIViewController {
+  var device: MTLDevice!
+  var metalLayer: CAMetalLayer!
+  var vertexBuffer: MTLBuffer!
+  var pipelineState: MTLRenderPipelineState!
+  var commandQueue: MTLCommandQueue!
 
+  let vertexData: [Float] = [
+    0.0, 1.0, 0.0,
+    -1.0, -1.0, 0.0,
+    1.0, -1.0, 0.0
+  ]
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    device = MTLCreateSystemDefaultDevice()
+
+    metalLayer = CAMetalLayer()
+    metalLayer.device = device
+    metalLayer.pixelFormat = .bgra8Unorm
+    metalLayer.framebufferOnly = true
+    metalLayer.frame = view.layer.frame
+    view.layer.addSublayer(metalLayer)
+
+    let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
+    vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [])
+
+    let defaultLibrary = device.newDefaultLibrary()!
+    let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
+    let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
+
+    let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+    pipelineStateDescriptor.vertexFunction = vertexProgram
+    pipelineStateDescriptor.fragmentFunction = fragmentProgram
+    pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+
+    pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+
+    commandQueue = device.makeCommandQueue()
+  }
 }
 
---
+++ Trapeze/Shaders.metal
@@ -0,0 +1,20 @@
+//
+//  Shaders.metal
+//  Trapeze
+//
+//  Created by Nikita Pokidyshev on 28.05.17.
+//  Copyright © 2017 Nikita Pokidyshev. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+vertex float4 basic_vertex(const device packed_float3* vertex_array [[ buffer(0) ]],
+                           unsigned int vid [[ vertex_id ]])
+{
+  return float4(vertex_array[vid], 1.0);
+}
+
+fragment half4 basic_fragment() {
+  return half4(1.0);
+}
